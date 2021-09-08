5bd503c61de6126f642697aa837c40ec6785c1c4
Dychkevych Kolya <dychkevych.kolya@gmail.com>
2021-09-08 16:57:43 +0300

    fixed menu / add hover on menu item / animated burger

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 import * as Style from './styled';
 import logo from '../../media/icon/logo.svg';
@@ -7,36 +7,51 @@ import close from '../../media/icon/close.svg';
 
 const Header = () => {
   const [menu, setMenu] = useState(false)
+  const [header, setHeader] = useState(false)
+
+  const listenScrollEvent = (event) => {
+    if (window.scrollY < 1) {
+      return setHeader(false)
+    } else if (window.scrollY > 1) {
+      return setHeader(true)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent);
+    return () =>
+      window.addEventListener('scroll', listenScrollEvent);
+  }, [])
 
   const showMenu = () => {
     setMenu(!menu)
   }
 
   return (
-   <>
-     <Style.HeaderWrap fixed={menu}>
-       <a className={'logo'} href="/"><img src={logo} alt="logo"/></a>
-       {
-         menu
-         ? <button onClick={showMenu} className={'closeMenu'}><img src={close} alt="icon"/></button>
-           :  <button onClick={showMenu} className={'menuBtn'}><img src={burger} alt="icon"/></button>
-       }
-     </Style.HeaderWrap>
+    <>
+      <Style.HeaderWrap fixed={menu} sticky={header} rotateBurger={menu}>
+        <a className={'logo'} href="/"><img src={logo} alt="logo"/></a>
+        <button onClick={showMenu} className={'menuBtn'}>
+          <div className="line"/>
+          <div className="line"/>
+        </button>
 
-     <Style.Menu show={menu}>
-       <div className="container-fluid">
-         <div className="row">
-           <div className="col-12">
-             <div className="nav d-flex flex-column">
-               <a href={'/projects'}>Проекты</a>
-               <a href={'/about'}>О нас</a>
-               <a href={'/contacts'}>Контакты</a>
-             </div>
-           </div>
-         </div>
-       </div>
-     </Style.Menu>
-   </>
+      </Style.HeaderWrap>
+
+      <Style.Menu show={menu}>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              <div className="nav d-flex flex-column">
+                <a href={'/projects'}>Проекты</a>
+                <a href={'/about'}>О нас</a>
+                <a href={'/contacts'}>Контакты</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Style.Menu>
+    </>
   )
 }
 
--- src/components/header/styled.js
+++ src/components/header/styled.js
@@ -2,18 +2,20 @@ import styled from 'styled-components';
 
 import bg from '../../media/image/menu_hover.png';
 import bg2 from '../../media/image/menu_hover2.png';
+import bg3 from '../../media/image/menu_hover3.svg';
 
 const HeaderWrap = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  position: ${props => props.fixed ? 'fixed' : 'absolute'};
+  position: fixed;
   padding: 0 42px;
   background-color: transparent;
   top: 42px;
   left: 0;
-  z-index: ${props => props.fixed ? '50' : 'none'};
+  z-index: 50;
+  overflow: hidden;
   .logo {
     width: 166px;
     height: 32px;
@@ -22,6 +24,8 @@ const HeaderWrap = styled.div`
     justify-content: center;
     cursor: pointer;
     z-index: 50;
+    transition: all .5s;
+    transform: ${props => props.sticky ? 'translateY(-200%)' : 'translateY(0)'};
     img {
       width: 100%;
       height: 100%;
@@ -35,17 +39,35 @@ const HeaderWrap = styled.div`
   .menuBtn {
     width: 64px;
     height: 64px;
-    background-color: transparent;
     border: none;
     display: flex;
     align-items: center;
     justify-content: center;
     cursor: pointer;
     z-index: 50;
+    background-color: ${props => props.sticky ? 'rgba(255, 255, 255, 0.1);' : 'transparent'};
+    border-radius: 50%;
+    backdrop-filter: blur(16px);
+    transition: background-color .5s;
+    flex-direction: column;
     img {
       width: 100%;
       height: 100%;
       object-fit: contain;
+    }
+    .line {
+      width: 28px;
+      height: 2px;
+      background-color: #fff;
+      transition: .5s;
+    }
+    & .line:first-child {
+      margin-bottom: ${props => props.rotateBurger ? '0' : '10px'};
+      transform: ${props => props.rotateBurger ? 'rotate(45deg)' : 'rotate(0deg)'};
+    }
+    & .line:last-child {
+      margin-top: ${props => props.rotateBurger ? '-2px' : '0'};
+      transform: ${props => props.rotateBurger ? 'rotate(-45deg)' : 'rotate(0deg)'};
     }
     @media(max-width: 575px) {
       width: 48px;
@@ -110,13 +132,27 @@ const Menu = styled.div`
      display: block;
      width: fit-content;
      z-index: 10;
-     transition: all .7s;
+     transition: all .4s;
      -webkit-text-stroke: 1px #fff;
      &:hover {
        color: transparent;
        -webkit-text-stroke: 1px #fff;
-       transition: all .5s;
+       transition: all .4s;
        z-index: 10;
+     }
+     &:after {
+       content: "";
+       position: absolute;
+       width: 100%;
+       min-height: 100vh;
+       left: 0;
+       top: 0;
+       background-image: url(${bg});
+       background-size: cover;
+       transition: .5s;
+       z-index: -1;
+       pointer-events: none;
+       opacity: 0;
      }
      @media(max-width: 575px){
        font-size: 28px;
@@ -124,40 +160,46 @@ const Menu = styled.div`
        margin-bottom: 40px;
      }
    }
-   }
    & a:last-child{
      margin-bottom: 0;
    }
- //   & a:first-child:hover{
- //      &:after {
- //        content: "";
- //        position: absolute;
- //        width: 100%;
- //        min-height: 100vh;
- //        background-image: url(${bg});
- //        left: 0;
- //        top: 0;
- //        background-size: cover;
- //        transition: .4s;
- //        z-index: 9;
- //     
- //   }
- // }
- //   & a:nth-child(2):hover{
- //     &:after {
- //       content: "";
- //       position: absolute;
- //       width: 100%;
- //       min-height: 100vh;
- //       background-image: url(${bg2});
- //       left: 0;
- //       top: 0;
- //       background-size: cover;
- //       transition: .4s;
- //       z-index: 9;
- //
- //     }
- //   }
+   a:first-child:hover{
+     &:after {
+       transition: .5s;
+       opacity: 1;
+     }
+   }
+ }
+   & a:nth-child(2):hover{
+     &:after {
+       content: "";
+       position: absolute;
+       width: 100%;
+       min-height: 100vh;
+       background-image: url(${bg2});
+       left: 0;
+       top: 0;
+       background-size: cover;
+       transition: .5s;
+       z-index: -1;
+      opacity: 1;
+     }
+   }
+  & a:nth-child(3):hover{
+    &:after {
+      content: "";
+      position: absolute;
+      width: 100%;
+      min-height: 100vh;
+      background-image: url(${bg3});
+      left: 0;
+      top: 0;
+      background-size: cover;
+      transition: .5s;
+      z-index: -1;
+      opacity: 1;
+    }
+  }
 `
 
 export {
